4c59d50f94f13ef213c41cc310309274214f3d38
Ryan Pilar <102194829+ryanpilar@users.noreply.github.com>
2024-03-03 23:01:11 -0700

    Added an error boundary to connectDB to deal with MONGO_URI missing

--- api/config/mongo.js
+++ api/config/mongo.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose')
 const connectDB = async () => {
+    
+    // Check if the MONGODB_URI is defined
+    if (!process.env.MONGODB_URI) {
+        console.error("Error: The MONGODB_URI environment variable is not defined.");
+        process.exit(1); // Exit with failure
+    }
+
     try {
         const connection = await mongoose.connect(process.env.MONGODB_URI)
         console.log(`MongoDB Connected: ${connection.connection.host}`)
     }
-    catch (err){
+    catch (err) {
         console.error(err)
         process.exit(1) // 1 indicates an unspecified error
     }
